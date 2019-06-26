f3c26e399d84478dc0720763f19ffb016996e96e
elaine mattos <elaine.mattos@outlook.pt>
2019-06-26 10:23:00 -0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return (
+      <div>
+        Chora boy
+      </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
